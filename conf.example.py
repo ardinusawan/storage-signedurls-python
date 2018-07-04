c16d9f0b38aacd022dd1ebe7a49997d94274faeb
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2018-07-04 16:41:27 +0700

    Update conf.example.py

--- conf.example.py
+++ conf.example.py
@@ -1,10 +1,10 @@
 import os.path
 
 # The email address for your GCS service account being used for signatures.
-SERVICE_ACCOUNT_EMAIL = ('abcdef1234567890@developer.gserviceaccount.com')
+SERVICE_ACCOUNT_EMAIL = ('merchant-upload-storage@mapan-merchant-dev.iam.gserviceaccount.com')
 
 # Bucket name to use for writing example file.
-BUCKET_NAME = 'bucket-name'
+BUCKET_NAME = 'stg-merchant-mapan-io'
 # Object name to use for writing example file.
 OBJECT_NAME = 'object.txt'
 
@@ -17,7 +17,10 @@ OBJECT_NAME = 'object.txt'
 # The default password for p12 file is `notasecret`
 # 
 # Given a GCS key in pkcs12 format, convert it to PEM using this command:
-#   openssl pkcs12 -in path/to/key.p12 -nodes -nocerts > path/to/key.pem
+#   openssl pkcs12 -in path/to/key.p12 -	nodes -nocerts > path/to/key.pem
 # Given a GCS key in PEM format, convert it to DER format using this command:
 #   openssl rsa -in privatekey.pem -inform PEM -out privatekey.der -outform DER
-PRIVATE_KEY_PATH = os.path.join(os.path.dirname(__file__), 'privatekey.der')
+# PRIVATE_KEY_PATH = os.path.join(os.path.dirname(__file__), 'privatekey.der')
+
+# BUT I'am using json key
+PRIVATE_KEY_PATH = os.path.join(os.path.dirname(__file__), 'service_account.json')
